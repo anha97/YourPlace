c48111f7047399351e9a3b69a9445dcd271a632e
anha97 <andrewhaftw@gmail.com>
2021-10-12 13:58:13 -0700

    Small fixes

--- backend/app.js
+++ backend/app.js
@@ -10,6 +10,17 @@ const app = express();
 
 app.use(bodyParser.json());
 
+// This is necessary for connection between frontend and backend
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  next();
+});
+
 app.use("/api/places", placesRoutes); // => /api/places/...
 
 app.use("/api/users", usersRoutes);
@@ -31,7 +42,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    "mongodb+srv://andrew:nBNYIUCj85kWVozt@cluster0.bthzl.mongodb.net/places?retryWrites=true&w=majority"
+    "mongodb+srv://andrew:nBNYIUCj85kWVozt@cluster0.bthzl.mongodb.net/mern?retryWrites=true&w=majority"
   )
   .then(() => {
     app.listen(5000); // If Mongoose is connected successfully, start the server
